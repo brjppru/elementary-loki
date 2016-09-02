0642633b462bce0e0774dbf95f199c178c07c082
Roman Y. Bogdanov <sam@brj.pp.ru>
2016-09-02 23:53:20 +0700

    2016-09-02-235319 fix some fucking errors

--- fix/dated.sh
+++ fix/dated.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+gsettings set com.canonical.indicator.datetime time-format '24-hour'
 gsettings set com.canonical.indicator.datetime time-format "custom"
 gsettings set com.canonical.indicator.datetime custom-time-format "%A, %d %B %Y, %I:%M"
+
+killall -HUP wingpanel
