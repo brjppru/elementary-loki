0070c173175413e594db750cd5d44a89866ebe1e
Roman Y. Bogdanov <sam@brj.pp.ru>
2017-04-30 00:50:28 +0700

    2017-04-30-005027 Don't push this commit

--- goods/bgimage/makeme.sh
+++ goods/bgimage/makeme.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# imagemagick tile same image
+
 convert 090127-2121-45.bmp -write mpr:tile  +delete -size 1366x768 tile:mpr:tile brj_wallpaper.jpg
 
 sudo cp brj_wallpaper.jpg /usr/share/backgrounds/
