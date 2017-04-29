71740c0e7a7a7e7bea7be90ad4c3362495f4b547
Roman Y. Bogdanov <sam@brj.pp.ru>
2017-04-29 18:48:55 +0700

    2017-04-29-184854 LAST time, Lukas, /dev/urandom IS NOT a variable name generator...

--- bin/brj.elementary.loki.sh
+++ bin/brj.elementary.loki.sh
@@ -31,19 +31,19 @@ beroot
 # nuke it!
 # =========================================================
 
-#sudo apt -y purge apparmor apparmor-utils noise pantheon-photos* audience evolution-data-server
-#sudo apt -y purge activity-log-manager-common activity-log-manager-control-center zeitgeist zeitgeist-core zeitgeist-datahub
-#sudo apt -y purge modemmanager appcenter
-#sudo apt -y purge sane pantheon-mail
-#sudo apt -y purge switchboard-plug-parental-controls
-#sudo apt -y purge apport apport-gtk apport-hooks-elementary apport-symptoms elementary-os-prerelease
-#sudo apt -y purge dropbox*
-#sudo apt -y purge elementaryos-report-problem-dockitem
-#sudo apt -y purge evolution-data-server-google2 evolution-data-server-outlook evolution-data-server-pantheon-online-accounts
-#sudo apt -y purge fonts-opendyslexic gsignond gsignond-extension-pantheon gsignond-plugin-lastfm gsignond-plugin-oauth
-#sudo apt -y purge libgsignon-glib1 libgsignond-common0 libpantheon-online-accounts0 pantheon-online-accounts-plugin-lastfm
-#sudo apt -y purge python3-problem-report switchboard-plug-parental-controls libaccounts-glib0 python3-apport
-#sudo apt -y purge apport apport-gtk
+sudo apt -y purge apparmor apparmor-utils noise pantheon-photos* audience evolution-data-server
+sudo apt -y purge activity-log-manager-common activity-log-manager-control-center zeitgeist zeitgeist-core zeitgeist-datahub
+sudo apt -y purge modemmanager appcenter
+sudo apt -y purge sane pantheon-mail
+sudo apt -y purge switchboard-plug-parental-controls
+sudo apt -y purge apport apport-gtk apport-hooks-elementary apport-symptoms elementary-os-prerelease
+sudo apt -y purge dropbox*
+sudo apt -y purge elementaryos-report-problem-dockitem
+sudo apt -y purge evolution-data-server-google2 evolution-data-server-outlook evolution-data-server-pantheon-online-accounts
+sudo apt -y purge fonts-opendyslexic gsignond gsignond-extension-pantheon gsignond-plugin-lastfm gsignond-plugin-oauth
+sudo apt -y purge libgsignon-glib1 libgsignond-common0 libpantheon-online-accounts0 pantheon-online-accounts-plugin-lastfm
+sudo apt -y purge python3-problem-report switchboard-plug-parental-controls libaccounts-glib0 python3-apport
+sudo apt -y purge apport apport-gtk
 
 # =========================================================
 # nuked locale
@@ -51,28 +51,26 @@ beroot
 
 sudo apt -y purge language-pack*
 sudo apt -y install language-pack-en language-pack-gnome-en language-pack-gnome-en-base
-sudo apt -y install language-pack-gnome-ru language-pack-ru-base
+sudo apt -y install language-pack-ru language-pack-gnome-ru language-pack-gnome-ru-base
 
 # =========================================================
 # begin up keys + ppa
 # =========================================================
 
-sudo apt -y update
-
-sudo apt-key net-update
-
-sudo apt -y dist-upgrade
-
 sudo service apparmor stop
 sudo update-rc.d -f apparmor remove
 
+sudo apt -y update
+sudo apt-key net-update
+sudo apt -y dist-upgrade
 sudo apt -y update
 
 # =========================================================
 # install needs
 # =========================================================
 
-sudo apt -y install apt-transport-https ca-certificates figlet dpkg deborphan dconf-editor python-gpgme software-properties-common software-properties-gtk debconf-utils
+sudo apt -y install apt-transport-https ca-certificates figlet dpkg deborphan dconf-editor python-gpgme
+sudo apt -y install software-properties-common software-properties-gtk debconf-utils
 sudo apt -y install aptitude ppa-purge bzip2 gdebi synaptic gdebi dconf-editor
 
 # =========================================================
@@ -117,10 +115,17 @@ figlet "add key's"
 #sudo add-apt-repository -y ppa:varlesh-l/indicator-kdeconnect
 #sudo add-apt-repository -y ppa:yunnxx/elementary
 #sudo add-apt-repository -y ppa:oibaf/graphics-drivers
-sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
 #sudo add-apt-repository -y ppa:atareao/pushbullet
 #sudo add-apt-repository -y ppa:plushuang-tw/uget-stable
 #sudo add-apt-repository -y ppa:git-core/ppa
+#sudo add-apt-repository -y ppa:wireshark-dev/stable
+#sudo add-apt-repository -y ppa:ricotz/docky
+#sudo add-apt-repository -y ppa:varlesh-l/loki
+#sudo add-apt-repository -y ppa:ubuntu-wine/ppa
+#sudo add-apt-repository -y ppa:ubuntuhandbook1/apps
+#sudo add-apt-repository -y ppa:quiterss/quiterss
+
+sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
 sudo add-apt-repository -y ppa:ubuntuhandbook1/corebird
 sudo add-apt-repository -y ppa:atareao/telegram
 sudo add-apt-repository -y ppa:libreoffice/ppa
@@ -129,20 +134,15 @@ sudo add-apt-repository -y ppa:transmissionbt/ppa
 sudo add-apt-repository -y ppa:videolan/stable-daily
 sudo add-apt-repository -y ppa:linrunner/tlp
 sudo add-apt-repository -y ppa:linphone/release
-sudo add-apt-repository -y ppa:wireshark-dev/stable
 sudo add-apt-repository -y ppa:papirus/papirus
-#sudo add-apt-repository -y ppa:ricotz/docky
-#sudo add-apt-repository -y ppa:varlesh-l/loki
 
-#sudo add-apt-repository -y ppa:ubuntu-wine/ppa
-#sudo add-apt-repository -y ppa:ubuntuhandbook1/apps
-#sudo add-apt-repository -y ppa:quiterss/quiterss
-
+# =========================================================
 # no ppa repos
+# =========================================================
 echo 'deb [trusted=yes] http://www.tataranovich.com/debian xenial main' > /etc/apt/sources.list.d/tataranovich.list
 echo 'deb [trusted=yes] http://archive.canonical.com/ubuntu xenial partner' > /etc/apt/sources.list.d/partner.list
 echo 'deb [trusted=yes arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
-echo 'deb [trusted=yes arch=amd64] https://repo.skype.com/deb stable main' > /etc/apt/sources.list.d/skype-stable.list
+#echo 'deb [trusted=yes arch=amd64] https://repo.skype.com/deb stable main' > /etc/apt/sources.list.d/skype-stable.list
 
 # =========================================================
 # begin up keys + ppa
@@ -162,18 +162,11 @@ echo wireshark-common wireshark-common/install-setuid boolean true | debconf-set
 sudo apt -y install tataranovich-keyring
 #
 sudo apt -y install aptitude cowsay curl dpkg aria2 zsh fonts-powerline git figlet lnav xkbset
-#sudo apt -y install android-tools-adb android-tools-fastboot bluetooth
-#sudo apt -y install libfuse-dev android-tools-adb
+sudo apt -y install android-tools-adb android-tools-fastboot bluetooth libfuse-dev android-tools-adb
 sudo apt -y install ssmtp whois zram-config
 sudo apt -y install gtk-redshift
 sudo apt -y install haveged molly-guard ncdu openssh-server htop powertop uget preload pollinate smartmontools ethtool fdupes
 
-# replace plank plank
-#sudo apt --purge remove -y plank
-#sudo apt install -y --reinstall plank
-#sudo apt -y install sni-qt hardcode-tray
-
-#
 # install my own
 #
 #sudo apt -y install wine
@@ -183,16 +176,26 @@ sudo apt -y install haveged molly-guard ncdu openssh-server htop powertop uget p
 #sudo apt -y install vdpau-va-driver libvdpau-va-gl1 libvdpau1 vlc browser-plugin-vlc
 #sudo apt -y install fluxgui
 #sudo apt -y install quiterss elinks liferea
+#sudo apt -y install mypaint 
+#sudo apt -y install glipper xclip xsel
+#sudo apt -y install pushbullet-indicator
+#sudo apt -y install smartmontools ethtool qt4-qtconfig dconf-tools
+#sudo apt -y install xserver-xorg-video-intel
+#sudo apt -y install hardinfo
+#sudo apt -y install gtk-recordmydesktop
+#sudo apt -y install audacity
+#sudo apt -y install indicator-kdeconnect libqt4-svg qt5ct
+#sudo apt -y install virt-manager netcat
 #
 sudo apt -y install unace unrar zip unzip xz-utils p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract wget curl
 sudo apt -y install zip unzip p7zip p7zip-rar rar unrar elinks
-sudo apt -y install gnome-system-monitor caffeine
+sudo apt -y install gnome-system-monitor caffeine netcat
 sudo apt -y install keepassx gmrun xdotool keepass2
 sudo apt -y install libreoffice libreoffice-gtk libreoffice-pdfimport libreoffice-avmedia-backend-gstreamer
 sudo apt -y install libreoffice-style-sifr libreoffice-lightproof-ru-ru libreoffice-help-ru libreoffice-l10n-ru
 sudo apt -y install ubuntu-restricted-extras ffmpeg x264 libdvdread4 lame vlc browser-plugin-vlc
-sudo apt -y install mypaint viewnior
-#sudo apt -y install elementary-wallpapers-extra comixcursors-righthanded-opaque comixcursors-righthanded
+sudo apt -y install viewnior
+sudo apt -y install elementary-wallpapers-extra comixcursors-righthanded-opaque comixcursors-righthanded
 sudo apt -y install transmission minissdpd natpmp-utils
 sudo apt -y install telegram
 sudo apt -y install tshark iftop
@@ -203,33 +206,25 @@ sudo apt -y install elementary-tweaks
 sudo apt -y install freerdp rdesktop
 sudo apt -y install tlp tlp-rdw
 sudo apt -y install sublime-text
-#sudo apt -y install glipper xclip xsel
-#sudo apt -y install pushbullet-indicator
 sudo apt -y install lame
 sudo apt -y install sqlite3
 sudo apt -y install torbrowser-launcher
-#sudo apt -y install smartmontools ethtool qt4-qtconfig dconf-tools
-#sudo apt -y install xserver-xorg-video-intel
 sudo apt -y install audacious audacious-plugins
 sudo apt -y install thunderbird thunderbird-gnome-support
 sudo apt -y install flashplugin-installer pepperflashplugin-nonfree
 sudo apt -y install linphone
 sudo apt -y install corebird
-#sudo apt -y install hardinfo
-#sudo apt -y install gtk-recordmydesktop
-#sudo apt -y install audacity
 sudo apt -y install gparted gnome-disk-utility usb-creator-gtk
 sudo apt -y install ntfs-config git zsh fonts-powerline
 sudo apt -y install systemd-ui
 sudo apt -y install google-chrome-stable mc
-#sudo apt -y install indicator-kdeconnect libqt4-svg qt5ct
 sudo apt -y install xfonts-terminus console-terminus ttf-dejavu ttf-liberation fonts-elementary-core fonts-droid-fallback
-#sudo apt -y install virt-manager netcat
 sudo apt -y install papirus-icon-theme
 
+# =========================================================
 # games
-#sudo apt -y install aisleriot gnome-cards-data pysolfc gnome-2048
-#sudo apt -y install dosbox
+# =========================================================
+#sudo apt -y install aisleriot gnome-cards-data pysolfc gnome-2048 dosbox
 
 # =========================================================
 # nuke archives
