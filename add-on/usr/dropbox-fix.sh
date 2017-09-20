1406493fe12189fc10cc241dc2601385c10eb277
Roman Y. Bogdanov <sam@brj.pp.ru>
2017-09-20 18:12:19 +0700

    2017-09-20-181218 Herp derp I left the debug in there and forgot to reset errors.

--- add-on/usr/dropbox-fix.sh
+++ add-on/usr/dropbox-fix.sh
@@ -5,7 +5,13 @@ sudo apt -y install git python-gpgme
 git clone https://github.com/zant95/elementary-dropbox /tmp/elementary-dropbox
 sudo bash /tmp/elementary-dropbox/install.sh
 
+cd /home/brj
+
 sudo chown -R brj:brj .dropbox-bin 
 sudo chown -R brj:brj .dropbox-dist
+
+cd /home/brj/bin
+curl -v https://linux.dropbox.com/packages/dropbox.py > /home/brj/bin/drb
+chmod 755 drb
 
 echo do chmod for `whoami`
